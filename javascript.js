cac2a98947bf6d8b572b685cf1459e969b02ff48
Azeem Pasha Shaik <108066377+Azeem-0@users.noreply.github.com>
2022-11-26 15:19:41 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,61 @@
+var d1,d2,d3,d4,d5;
+document.addEventListener("keypress",function(event){
+    switch (event.key) {
+        case "a":
+            d1 = new Audio("Drum_sounds/drum1.mp3");
+            d1.play();
+            break;
+        case "d":
+            d2 = new Audio("Drum_sounds/drum2.mp3");
+            d2.play();
+            break;
+        case "f":
+            d3 = new Audio("Drum_sounds/drum3.mp3");
+            d3.play();
+            break;
+        case "l":
+            d4 = new Audio("Drum_sounds/drum4.mp3");
+            d4.play();
+            break;   
+        case "j":
+            d5 = new Audio("Drum_sounds/drum5.mp3");
+            d5.play();
+            break;   
+        default:
+            break;
+    }
+
+});
+for(var i=0;i<6;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var c = this.innerHTML;
+        console.log(c);
+        switch (c) {
+            case "A":
+                d1 = new Audio("Drum_sounds/drum1.mp3");
+                    d1.play();
+                    break;
+            case "D":
+                d2 = new Audio("Drum_sounds/drum2.mp3");
+                    d2.play();
+                    break;
+            case "F":
+                d3 = new Audio("Drum_sounds/drum3.mp3");
+                    d3.play();
+                    break;
+            case "J":
+                d4 = new Audio("Drum_sounds/drum4.mp3");
+                    d4.play();
+                    break;
+            case "L":
+                    d5 = new Audio("Drum_sounds/drum5.mp3");
+                    d5.play();
+                    break;
+            default:
+                break;
+        }
+    });
+}
+
+
